bffd6917b3e52c92fa61e6150446cc4f23d1050a
Elena Albu <ealbu@KRO.LOCAL>
2023-03-23 19:26:35 +0200

    tot

--- src/main/java/curs9/GitCommanda.java
+++ src/main/java/curs9/GitCommanda.java
@@ -4,8 +4,12 @@ public class GitCommanda {
  /*	
  Pas 1. git init -> initializeaza un git repository
  pas 2. adaugam un fisier .gitignore
- pas r git add .
- git commit -m "mesaj"
+ pas 3. git add .
+ pas 4.git commit -m "mesaj"
+ 
+ Pana aici am terminat cu repoul local. Acum tb sa facem legatura cu repo remote
+ Mergem pe github su facem repo nou
+ Rulam comanda 
  pas 5 git remote add origin <URL>
  pas 6 git push origin master 
  
