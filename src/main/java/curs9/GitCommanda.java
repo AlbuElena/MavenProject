acb9135642090676ecf71fbffe9114c9b7153265
Elena Albu <ealbu@KRO.LOCAL>
2023-03-23 19:24:42 +0200

    gitignore

---
+++ src/main/java/curs9/GitCommanda.java
@@ -0,0 +1,13 @@
+package curs9;
+
+public class GitCommanda {
+ /*	
+ Pas 1. git init -> initializeaza un git repository
+ pas 2. adaugam un fisier .gitignore
+ pas r git add .
+ git commit -m "mesaj"
+ pas 5 git remote add origin <URL>
+ pas 6 git push origin master 
+ 
+*/
+}
