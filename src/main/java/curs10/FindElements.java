6ce91f42184cb454320be7aec49743a0ac024324
Elena Albu <ealbu@KRO.LOCAL>
2023-03-24 18:04:05 +0200

    curs 10 adaugat comentarii

--- src/main/java/curs10/FindElements.java
+++ src/main/java/curs10/FindElements.java
@@ -1,5 +1,24 @@
 package curs10;
 
-public class FindElements {
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import utils.BaseTest;
+
+public class FindElements extends BaseTest{
+	
+	
+	@Test
+	public void findElements() {
+		
+		List<WebElement> books = driver.findElements(By.cssSelector("h3[class*='sc_title']"));
+		System.out.println(books.size());
+		books.get(2).click();
+		
+		
+	}
 
 }
---
+++ src/main/java/curs9/GitCommands.java
@@ -0,0 +1,22 @@
+package curs9;
+
+public class GitCommands {
+ /*	
+ Pas 1. git init -> initializeaza un git repository si se ruleaza o singura data
+ pas 2. adaugam un fisier .gitignore
+ pas 3. git add .
+ pas 4.git commit -m "mesaj"
+ 
+ Pana aici am terminat cu repoul local. Acum tb sa facem legatura cu repo remote
+ Mergem pe github si facem repo nou
+ Rulam comanda de mai jos in git bash pt a face legatura:
+ 
+ pas 5 git remote add origin https://github.com/KeyTraining-Dragos/MavenProject.git
+ 
+  	 * Dupa acest pas avem legatura facuta intre local si remote 
+	 * Urmeaza sa urcam codul de pe local pe remote
+	 
+ pas 6 git push origin master 
+ 
+*/
+}
