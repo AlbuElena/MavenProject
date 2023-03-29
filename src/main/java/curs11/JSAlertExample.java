23a99015d035aac8f41b49d7d4b5968261779e30
Elena Albu <ealbu@KRO.LOCAL>
2023-03-29 10:43:15 +0300

    curs 11 again

---
+++ src/main/java/curs11/JSAlertExample.java
@@ -0,0 +1,33 @@
+package curs11;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import utils.BaseTest;
+
+public class JSAlertExample extends BaseTest{
+
+	@Test
+	public void simpleJsAlert() throws InterruptedException {
+		
+			driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+		
+			driver.findElement(By.cssSelector("button[onclick*=\"jsAle\"]")).click();
+			Thread.sleep(3000);	
+				
+			//driver.switchTo().alert().accept(); -- face switch catre alerta popup si accept apasa bttn ok, iar dismiss da click pe cancel
+				
+			Alert alertJs = driver.switchTo().alert();
+			alertJs.accept();
+				
+			WebElement result = driver.findElement(By.cssSelector("p[id='result']"));
+			assertEquals(result.getText(), "You successfully clicked an alert");
+				
+				
+	}
+}
