7680b44008960f4eb02f04dfefde1be6bbd35a57
unknown <elena.albu@alten.com>
2023-04-03 09:53:39 +0300

    locator Tema 3 curs12

--- src/main/java/pageObjects/MenuPage.java
+++ src/main/java/pageObjects/MenuPage.java
@@ -18,6 +18,7 @@ public class MenuPage {
 	public By contactsLink = By.linkText("CONTACTS");
 	//locator pt ex1 din tema:
 	public By singleAuthorLink = By.linkText("SINGLE AUTHOR");
+	public By searchField = By.cssSelector("button[title = 'Open search']");
 	
 	//driver.findElement(By.linkText("Login")).click();
 	// indentificare element                  + actiune pe element
